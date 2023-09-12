5cb139599a3850d2522adfcbd3d225263b025039
Niraj Niroula <niniroula@gmail.com>
2023-09-12 23:23:34 +0545

    revamp bottom tab navigator

--- App.jsx
+++ App.jsx
@@ -1,16 +1,18 @@
 // React Native Bottom Navigation
 // https://aboutreact.com/react-native-bottom-navigation/
-import * as React from 'react';
+import * as React from "react";
 
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import { TabFloatButton } from "./src/components/TabFloatButton";
+import { Colors } from "./src/utils/colors";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -19,7 +21,8 @@ function HomeStack() {
   return (
     <Stack.Navigator
       initialRouteName="Home"
-      screenOptions={{headerShown: false}}>
+      screenOptions={{ headerShown: false }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
@@ -30,7 +33,8 @@ function SettingsStack() {
   return (
     <Stack.Navigator
       initialRouteName="Settings"
-      screenOptions={{headerShown: false}}>
+      screenOptions={{ headerShown: false }}
+    >
       <Stack.Screen name="Settings" component={SettingsScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
@@ -42,20 +46,26 @@ function App() {
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Feed"
-        screenOptions={({route}) => ({
-          headerStyle: {backgroundColor: '#42f44b'},
-          headerTintColor: '#fff',
-          headerTitleStyle: {fontWeight: 'bold'},
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-          tabBarIcon: ({focused, color, size}) => {
+        screenOptions={({ route }) => ({
+          headerStyle: { backgroundColor: "tomato" },
+          tabBarStyle: {
+            backgroundColor: Colors.PAGE_WHITE,
+            borderTopWidth: 0,
+            elevation: 0,
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+          tabBarActiveBackgroundColor: Colors.BOTTOM_TAB_BAR_BG,
+          tabBarInactiveBackgroundColor: Colors.BOTTOM_TAB_BAR_BG,
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-            if (route.name === 'HomeStack') {
-              iconName = focused ? 'home-circle' : 'home-circle-outline';
-            } else if (route.name === 'SettingsStack') {
-              iconName = focused
-                ? 'account-settings'
-                : 'account-settings-outline';
+            if (route.name === "HomeStack") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "ProfileStack") {
+              iconName = focused ? "account-circle" : "account-circle-outline";
             }
             return (
               <MaterialCommunityIcons
@@ -65,21 +75,33 @@ function App() {
               />
             );
           },
-        })}>
+        })}
+      >
         <Tab.Screen
           name="HomeStack"
           component={HomeStack}
           options={{
-            tabBarLabel: 'Home',
-            title: 'Home',
+            tabBarLabel: "Home",
+            title: "Home",
           }}
         />
+
         <Tab.Screen
-          name="SettingsStack"
+          name="Rocket"
           component={SettingsStack}
           options={{
-            tabBarLabel: 'Settings',
-            title: 'Setting',
+            tabBarButton: (props) => (
+              <TabFloatButton bgColor={Colors.BOTTOM_TAB_BAR_BG} {...props} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="ProfileStack"
+          component={SettingsStack}
+          options={{
+            tabBarLabel: "Profile",
+            title: "Profile",
           }}
         />
       </Tab.Navigator>
