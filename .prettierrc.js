4099389d7afd51fe85aab3df73b5aa47d8d6be38
Niraj Niroula <niniroula@gmail.com>
2023-09-22 22:07:22 +0545

    add rn paper, minor UI changes

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,7 @@
 module.exports = {
-  bracketSpacing: true,
-  trailingComma: "all",
-  endOfLine: "auto",
-  semi: true,
-  singleQuote: false,
-  printWidth: 100,
-  tabWidth: 4,
+        bracketSpacing: true,
+        trailingComma: "all",
+        endOfLine: "auto",
+        semi: true,
+        singleQuote: false,
 };
